e624f1f2c1cfde313df59045421a62df5f5a03cd
Oluwatobi Omotayo <tobitech@ymail.com>
2022-08-18 11:35:47 +0100

    Initial Commit

---
+++ MapSearch/MapSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapSearchApp.swift
+//  MapSearch
+//
+//  Created by Oluwatobi Omotayo on 18/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MapSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
