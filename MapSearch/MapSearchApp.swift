e5d2562cdbbb301eeef71c47af0e161f6a3b91fd
Oluwatobi Omotayo <tobitech@ymail.com>
2022-08-18 11:37:26 +0100

    project setup and initial commit

--- MapSearch/MapSearchApp.swift
+++ MapSearch/MapSearchApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct MapSearchApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
