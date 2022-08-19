4d98f95bea741b8348e5b71e379ebb52d931a828
Oluwatobi Omotayo <tobitech@ymail.com>
2022-08-19 10:57:45 +0100

    explore the MKLocalSearch API on the playground

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -9,6 +9,16 @@ class LocalSearchCompleterDelegate: NSObject, MKLocalSearchCompleterDelegate {
   func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
     print("succeeded")
     dump(completer.results)
+    
+    // this is lazy so we need to kickstart a search for it to start the search
+    let search = MKLocalSearch(request: .init(completion: completer.results[0]))
+    Task {
+      let response = try await search.start()
+      print(response)
+      
+      response.mapItems[0].placemark.coordinate
+      response.boundingRegion
+    }
   }
   
   func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
@@ -21,4 +31,6 @@ completer.delegate = delegate
 
 completer.queryFragment = "Apple Store"
 
+
+// MKLocalSearch.init(request: .init(coordinateRegion: <#T##MKCoordinateRegion#>))
 
