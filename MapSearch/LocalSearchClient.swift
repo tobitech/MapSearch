6dd9686b129e6b009f1abeba35881ffd5a10adab
Oluwatobi Omotayo <tobitech@ymail.com>
2022-08-19 11:35:24 +0100

    Create an extension on Effect that can wrap an async operation and handle the response and error.
    This is so that we can take advantage of other available APIs that support async operations whenever we need to with the composable architecture.

--- MapSearch/LocalSearchClient.swift
+++ MapSearch/LocalSearchClient.swift
@@ -8,22 +8,48 @@ struct LocalSearchClient {
 extension LocalSearchClient {
   static let live = Self(
     search: { completion in
-      Effect.future { callback in
-        
-        // spin up a new async context.
-        // Task is a unit of asynchronous work.
-        // This was introduced in Swift 5 iOS 13+
-        Task {
-          do {
-            let response = try await MKLocalSearch(request: .init(completion: completion))
-              .start()
-            callback(.success(response))
-          } catch {
-            callback(.failure(error))
-          }
-        }
-        
+      
+      Effect.task {
+        try await MKLocalSearch(request: .init(completion: completion))
+          .start()
       }
+      
+//      Effect.future { callback in
+//
+//        // spin up a new async context.
+//        // Task is a unit of asynchronous work.
+//        // This was introduced in Swift 5 iOS 13+
+//        Task {
+//          do {
+//            let response = try await MKLocalSearch(request: .init(completion: completion))
+//              .start()
+//            callback(.success(response))
+//          } catch {
+//            callback(.failure(error))
+//          }
+//        }
+//      }
+      
     }
   )
+}
+
+extension Effect {
+  static func task(
+    priority: TaskPriority? = nil,
+    operation: @escaping @Sendable () async throws -> Output
+  ) -> Effect
+  where Failure == Error {
+    .future { callback in
+      Task(priority: priority) {
+        do {
+          let response = try await operation()
+          callback(.success(response))
+        } catch {
+          callback(.failure(error))
+        }
+      }
+      
+    }
+  }
 }
